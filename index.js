e57dcdd0f671858d2a54128197fffbc536398254
Armxxn34 <73848907+Armxxn34@users.noreply.github.com>
2021-12-04 19:39:14 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,33 @@
 
 const Discord = require('discord.js')
 const fs = require('fs');
-
 const { Client, Collection, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const { token, guildId } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.data.name, command);
 }
 
 client.once('ready', () => {
-	console.log('Ready!');
+require('./connectToMongo.js')
 console.log('online')
      const activities = [
     `/help`,
-    `coins`,
-    `CoinHome Support`
+    `money`,
+    `Mr Money Support`
     //y'all can add more ig
   ];
   let i = 0;
   setInterval(() => client.user.setActivity(`${activities[i++ % activities.length]}`, { type: `LISTENING` }), 5000);
 });
-
 client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
+const command = client.commands.get(interaction.commandName);
 
 	if (!command) return; 
 const timer = ms => new Promise( res => setTimeout(res, ms));
@@ -47,6 +42,7 @@ if(command.cooldowns != undefined && command.cooldown == undefined || command.co
                       .setDescription(`Seems your on cooldown, you only need to wait ${command.cooldown}s`)
                         .setColor('RANDOM')
                     return await interaction.reply({embeds: [CooldownEmbed]})
+                      console.log(cooldowns)
                 }
             }
 
@@ -59,11 +55,11 @@ if(command.cooldowns != undefined && command.cooldown != undefined){
                     await timer(command.cooldown * 1000)
         
                     command.cooldowns.delete(interaction.member.id)
-}
+}               
 	} catch (error) {
 		console.error(error)
-		return interaction.reply({ content: 'There was an error while executing this command!\n If you want, you can send the following error to our support server.\n```js'+ error + '```', ephemeral: true });
+		return interaction.reply({ content: 'There was an error while executing this command!\n If you want, you can send the following error to our support server.\n``` js '+ error + '```', ephemeral: true, });
+
 	}
 });
-
 client.login(token);
