54930cd677c762852eea552ce5d4d0d188fd302b
Armxxn34 <73848907+Armxxn34@users.noreply.github.com>
2021-12-04 19:42:59 +0000

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,12 @@
+client.once('ready', () => {
+require('./connectToMongo.js')
+console.log('online')
+     const activities = [
+    `/help`,
+    `money`,
+    `Mr Money Support`
+    //y'all can add more ig
+  ];
+  let i = 0;
+  setInterval(() => client.user.setActivity(`${activities[i++ % activities.length]}`, { type: `LISTENING` }), 5000);
+});
