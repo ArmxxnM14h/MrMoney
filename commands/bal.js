4c6b2775cb2d6f3e10bd95658a42822b83424e1b
Armxxn34 <73848907+Armxxn34@users.noreply.github.com>
2022-02-06 14:14:41 +0000

    Update bal.js

--- commands/bal.js
+++ commands/bal.js
@@ -1,8 +1,6 @@
-// Defining Random Stuff
-const { SlashCommandBuilder } = require("@discordjs/builders");
-const { MessageEmbed } = require("discord.js");
-const db = require("quick.db");
-
+const schema = require("../userschema.js");
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 // All the command info will be listed here
 module.exports = {
   data: new SlashCommandBuilder()
@@ -11,48 +9,53 @@ module.exports = {
     .addUserOption((option) =>
       option
         .setName("user")
-        .setDescription("Get another user's balance!") 
+        .setDescription("Get another user's balance!")
         .setRequired(false),
-        
-),
-cooldowns : new Set(),
-cooldown : 5,
-// Executing the interaction and defining nessessery stuff
+
+    ),
+  cooldowns: new Set(),
+  cooldown: 5,
+  // Executing the interaction and defining nessessery stuff
   async execute(interaction) {
     const user = interaction.options.getUser("user");
 
+
     if (user) {
-      const bal = db.fetch(`${user}_wallet`) || 0;
-      const bank = db.fetch(`${user}_bank`) || 0;
-     const networth = bank + bal
-      const balEmbed = new MessageEmbed()
-        .setColor("GREEN")
-        .setTitle(`${user.tag}'s Balance'`)
-        .setDescription(` :purse: Wallet: $${bal}
+schema.findOne({
+        userID: user.id
+      }, async (err, res) => {
+        if (err) console.log(err);
 
-:bank: Bank: $${bank}
- 
-:money_mouth: Networth: $${networth}`)
-        .setTimestamp();
+if(!res){
+          const newDoc = new schema({
+            userID: interaction.user.id,
+            userName: interaction.user.username,
+            serverID: interaction.guild.id,
+            coins: 100
+          });
+newDoc.save().catch(err => console.log(err));
 
-// Reply to the entire interaction
-      await interaction.reply({ embeds: [balEmbed] });
-    } else {
-      const bal = db.fetch(`${interaction.user.username}_wallet`) || 0;
-     const bank = db.fetch(`${interaction.user.username}_bank`) || 0;
-     const networth = bank + bal || 0;
-// Entirely new embed
-      const balEmbed = new MessageEmbed()
-        .setColor("GREEN")
-        .setTitle("Your Balance:")
-        .setDescription(`:purse: Wallet: $${bal} 
+          const balEmbed = new MessageEmbed()
+          .setColor("GREEN")
+          .setTitle(`${user.tag}'s Balance'`)
 
-:bank: Bank: $${bank}
+          .setDescription(`$${res.coins}`)
+          .setTimestamp();
 
-:money_mouth: NetWorth: $${networth}`)
-        .setTimestamp();
-// Another reply and ending off the command
-      await interaction.reply({ embeds: [balEmbed] });
+          // Reply to the entire interaction
+          await interaction.reply({ embeds: [balEmbed] });
+        } else {
+
+          const balEmbed = new MessageEmbed()
+          .setColor("GREEN")
+          .setTitle(`${user.tag}'s Balance'`)
+          .setDescription(`:purse: Wallet: $${res.coins}`)
+          .setTimestamp();
+
+          // Reply to the entire interaction
+          await interaction.reply({ embeds: [balEmbed] });
+        }
+      });
     }
-  },
-};
+  }
+}
