cb6e0aea3f0a3a3f9d705d2257d3fa1261990626
Armxxn34 <73848907+Armxxn34@users.noreply.github.com>
2022-02-08 17:07:52 +0000

    Update withdraw.js

--- commands/withdraw.js
+++ commands/withdraw.js
@@ -1,45 +1,71 @@
 // Defining Random Stuff
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageEmbed } = require("discord.js");
-const db = require("quick.db");
-
+const schema = require("../models/userschema.js");
 // All the command info will be listed here
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("with")
-    .setDescription("Withdraw cash out of your bank")
+    .setDescription("withdraw cash from your bank")
     .addIntegerOption((option) =>
       option
         .setName("amount")
-        .setDescription("choose the amount you want to withdraw")
-        .setRequired(true),
+        .setDescription("choose the amount you want to deposit")
+        .setRequired(true)
     ),
-  cooldowns: new Set(),
-  cooldown: 6,
-  // Executing the interaction and defining nessessery stuff
+cooldowns : new Set(),
+cooldown : 5, 
+// Executing the interaction and defining nessessery stuff
   async execute(interaction) {
-    const user = interaction.options.getUser("user")
+const user = interaction.options.getUser("user")
     const subtract = interaction.options.getInteger("amount");
-    const bank = db.fetch(`${interaction.user.username}_bank`)
-    if (bank < subtract) {
-      const ErrorEmbed = new MessageEmbed()
-        .setTitle('Error In Transaction')
-        .setDescription('**Phone:** You dont seem to have enough money in your bank to withdraw')
+schema.findOne({
+			userID: interaction.user.id
+		}, (err, res) => {
+			if (err) console.log(err);
+
+if(!res){
+const errEmbed = new MessageEmbed()
+.setTitle('Error...')
+.setDescription('First time users must execute the bal command before using other commands')
+.setColor('RANDOM')
+}
+
+    if (subtract <= 0) {
+      const Abuser = new MessageEmbed()
+        .setTitle('Unable to withdraw')
+        .setDescription('You cannot withdraw anything under 0')
         .setColor('RANDOM')
-      return interaction.reply({ embeds: [ErrorEmbed], ephemeral: true })
+      return interaction.reply({ embeds: [Abuser], ephemeral: true })
+
+    } else if (res.bank < subtract) {
+const ErrorEmbed = new MessageEmbed()
+.setTitle('Error In Transaction')
+.setDescription('**Text:** Your balance is too low to transfer your money to the bank')
+.setColor('RANDOM')
+      return interaction.reply({ embeds: [ErrorEmbed], ephemeral: true})
     }
 
-    else if (bank > subtract) {
-      // Entirely new embed
-      const deposit = db.subtract(`${interaction.user.username}_bank`, subtract) || 0;
-      const bank = db.add(`${interaction.user.username}_wallet`, subtract) || 0;
+ else if (res.bank > subtract) {
+// Entirely new embed
+      res.coins = res.coins + subtract
+      res.bank = res.bank - subtract
+res.save();
       const balEmbed = new MessageEmbed()
         .setColor("GREEN")
-        .setTitle(`${interaction.user.username}`)
-        .setDescription(`**Phone:** $${subtract}  has been added to your wallet, You now have $${bank} in your wallet`)
-        .setTimestamp();
+        .setTitle(`Bank Withdrawal`)
+        .setDescription(` Cash has successfully been taken out:
 
-      await interaction.reply({ embeds: [balEmbed] });
+Amount of cash taken out: **$${subtract}**
+
+Wallet balance: **$${res.coins}**
+
+Current bank balance: **$${res.bank}**
+`)
+        .setTimestamp();
+        
+       interaction.reply({ embeds: [balEmbed] });
     }
+  });
   }
-}
+}          
