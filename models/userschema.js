47f887211b5cf597c48e1dea8530f2193ac0df6f
SathvikHegdee <sathushirnika@gmail.com>
2022-02-16 22:40:52 +0530

    fix formatting

--- models/userschema.js
+++ models/userschema.js
@@ -4,7 +4,7 @@ const userschema = mongoose.Schema({
   userID: String,
   userName: String,
   coins: { type: Number, default: 100 },
-  bank: { type: Number,  default: 0    }
+  bank: { type: Number,  default: 0 }
 });
 
 module.exports = mongoose.model("User", userschema);
