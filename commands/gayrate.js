74942029b734feb954562807eb94c2a4b3e79bb3
Armxxn34 <73848907+Armxxn34@users.noreply.github.com>
2022-03-16 17:42:49 +0000

    Update gayrate.js

--- commands/gayrate.js
+++ commands/gayrate.js
@@ -10,6 +10,7 @@ cooldowns : new Set(),
 	    cooldown : 8,
 	async execute(interaction, client) {
 const gay = Math.floor(Math.random() * Math.floor(100));
+if(interaction.user.id === "465945329968218144" gay = "0%")
         const pingy = new MessageEmbed()
 	.setColor('RANDOM')
 	.setTitle("Gay Scale...")
