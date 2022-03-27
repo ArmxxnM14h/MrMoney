c486ea4ef7775600537121f8cf5acff4c765f842
Armxxn34 <73848907+Armxxn34@users.noreply.github.com>
2022-03-27 08:45:54 +0100

    Update rob.js

--- commands/rob.js
+++ commands/rob.js
@@ -1,7 +1,7 @@
 // Defining Random Stuff
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageEmbed } = require("discord.js");
-const db = require("quick.db");
+const schema = require("../models/userschema.js");
 
 // All the command info will be listed here
 module.exports = {
@@ -13,51 +13,79 @@ module.exports = {
         .setName("user")
         .setDescription("The person you want to rob from")
         .setRequired(true)
-),
-        cooldowns : new Set(),
-	    cooldown : 5,
+    ),
+  cooldowns: new Set(),
+  cooldown: 5,
   // Executing the interaction and defining nessessery stuff
   async execute(interaction) {
     const user = interaction.options.getUser("user");
-    const bal = db.fetch(`${user.username}_wallet`);
-const AttemptedRobbery = db.fetch(`${interaction.user.username}_wallet`)
-    const chance = Math.floor(Math.random() * Math.floor(100));
-  if (interaction.user.id === user.id) return;
-   else if (bal < 50) {
-      const AnotherOne = new MessageEmbed()
-        .setTitle("Bruh..")
-        .setDescription(
-          `Why are you robbing users with a balance of less then 50??`
-        )
-        .setColor("RANDOM");
 
-      await interaction.reply({ embeds: [AnotherOne], ephemeral: true });
-    } else if (AttemptedRobbery < 500) {
-const BrokeBoi = new MessageEmbed()
-.setTitle('Your broke')
-.setDescription(`You need minimum $500 in your wallet to rob dude`)
-.setColor('RANDOM')
-await interaction.reply({embeds: [BrokeBoi]})
-} else if (chance < 50) {
-      const ErrorEmbed = new MessageEmbed()
-        .setTitle("Yikes...")
-        .setDescription(
-          "You really failed the robbery and lost a total of $500")
-        .setColor("RANDOM");
- db.subtract(`${interaction.user.username}_wallet`, 500)
-      await interaction.reply({ embeds: [ErrorEmbed]});
-    } else if (chance > 50) {
-      // Entirely new embed
-const Ampro = Math.floor(Math.random() * Math.floor(bal));
-      const RobberySuccess = new MessageEmbed()
-        .setColor("RANDOM")
-        .setTitle(`Success!`)
-        .setDescription(
-          `You robbed a ${user.username} of $${Ampro}, you are so sad..`
-        )
-        .setTimestamp();
-      await interaction.reply({ embeds: [RobberySuccess] });
-db.subtract(`${user.username}_wallet`, Ampro)
-    } 
+    if (interaction.user.id === user.id) return interaction.reply("You cannot rob yourself!");
+
+    schema.findOne({
+      userID: interaction.user.id
+    }, async (err, res) => {
+      if (err) console.log(err);
+
+      schema.findOne({
+        userID: user.id
+      }, async (err2, res2) => {
+        if (err2) console.log(err2);
+
+        if (!res2) {
+          const errEmbed = new MessageEmbed()
+            .setColor("RED")
+            .setDescription(`${user.username} hasn't used the bot yet!!`)
+            .setTimestamp();
+
+          // Reply to the entire interaction
+          return interaction.reply({ embeds: [errEmbed] });
+        }
+
+        if (res2.coins < 50) {
+          const AnotherOne = new MessageEmbed()
+            .setTitle("Bruh..")
+            .setDescription(
+              `Why are you robbing users with a balance of less then 50??`
+            )
+            .setColor("RANDOM");
+
+          return await interaction.reply({ embeds: [AnotherOne], ephemeral: true });
+        } else if (res.coins < 500) {
+          const BrokeBoi = new MessageEmbed()
+            .setTitle('Your broke')
+            .setDescription(`You need minimum $500 in your wallet to rob dude`)
+            .setColor('RANDOM')
+          return await interaction.reply({ embeds: [BrokeBoi] });
+        }
+
+        const chance = Math.floor(Math.random() * Math.floor(100));
+
+        if (chance < 50) {
+          const ErrorEmbed = new MessageEmbed()
+            .setTitle("Yikes...")
+            .setDescription(
+              "You really failed the robbery and lost a total of $500")
+            .setColor("RANDOM");
+          res.coins = res.coins - 500;
+          res.save().catch(err => console.log(err));
+          return await interaction.reply({ embeds: [ErrorEmbed] });
+        } else if (chance >= 50) {
+          const Ampro = Math.floor(Math.random() * Math.floor(res2.coins));
+          const RobberySuccess = new MessageEmbed()
+            .setColor("RANDOM")
+            .setTitle(`Success!`)
+            .setDescription(
+              `You robbed a ${user.username} of $${Ampro}, you are so bad..`
+            )
+            .setTimestamp();
+          res.coins = res.coins + Ampro;
+          res.save().catch(err => console.log(err));
+          res2.coins = res2.coins - Ampro;
+          res2.save().catch(err => console.log(err));
+          return await interaction.reply({ embeds: [RobberySuccess] });
+        }
+      });
+    });
   },
 };
