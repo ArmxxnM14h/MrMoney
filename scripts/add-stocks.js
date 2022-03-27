60b55bcbd2634893ca1461eb8ffeb5c65c4dd2d0
SathvikHegdee <sathushirnika@gmail.com>
2022-03-27 23:03:09 +0530

    Move standlone commands to scripts folder

---
+++ scripts/add-stocks.js
@@ -0,0 +1,12 @@
+require("../connectToMongo.js");
+const schema = require("../models/stockschema.js");
+
+const newDoc = new schema({
+  stockID: "MC",
+  stockName: "Monke Coin",
+  currentprice: 10,
+  priceTable: [0],
+  changePercent: 0,
+  volume: 0
+});
+newDoc.save().catch(err => console.log(err)).then(() => console.log("Added Monke Coin to database"));
