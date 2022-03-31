f97c884196c342dbcb041c4efc0e989c6202f97d
SathvikHegdee <sathushirnika@gmail.com>
2022-03-31 23:00:26 +0530

    fix bug in stockinfo

--- commands/stockinfo.js
+++ commands/stockinfo.js
@@ -7,18 +7,18 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('stockinfo')
 		.setDescription('View info about the current stocks.')
-    .addStringOption((option) =>
-      option
-        .setName("stockname")
-        .setDescription("View more details about a stock")
-        .setRequired(false),
-    ),
+		.addStringOption((option) =>
+			option
+				.setName("stockname")
+				.setDescription("View more details about a stock")
+				.setRequired(false),
+		),
 	cooldowns: new Set(),
 	cooldown: 5,
 	async execute(interaction) {
 		const stockinfo = interaction.options.getString("stockname");
 
-    if(!stockinfo) {
+		if (!stockinfo) {
 			schema.find({}, async (err, res) => {
 				if (err) console.log(err);
 				const stockembed = new MessageEmbed()
@@ -26,19 +26,19 @@ module.exports = {
 					.setColor("#0099ff")
 					.setDescription("**Here are the current stocks:**");
 
-				for(let i = 0; i < res.length; i++) {
+				for (let i = 0; i < res.length; i++) {
 					stockembed.addField(`${i + 1}. ${res[i].stockName}`, `Stock ID: ${res[i].stockID}\nCurrent Price: $${res[i].currentprice}\nChange Percent: ${res[i].changePercent}%\nVolume: ${res[i].volume}`);
 				}
 
 				return await interaction.reply({ embeds: [stockembed] });
 			});
-		} else if(stockinfo) {
+		} else if (stockinfo) {
 			schema.findOne({
 				stockName: stockinfo
 			}, async (err, res) => {
 				if (err) console.log(err);
 
-				if(!res) {
+				if (!res) {
 					return await interaction.reply("That stock does not exist.");
 				}
 
@@ -46,18 +46,27 @@ module.exports = {
 					.setTitle(res.stockName)
 					.setColor("#0099ff")
 					.setDescription(`**Stock ID:** ${res.stockID}\n**Current Price:** $${res.currentprice}\n**Change Percent:** ${res.changePercent}%\n**Volume:** ${res.volume}`);
-				
+
 				const chart = new Chart();
 				chart
 					.setConfig({
 						type: 'line',
-						data: { datasets: [{label: "Monke Coin", data: res.priceTable }] },
+						data: {
+							labels: res.priceTable.map((_, i) => i),
+							datasets: [
+								{
+									label: "Price",
+									data: res.priceTable
+								}
+							]
+						},
 					})
-					.setWidth(400)
-					.setHeight(200)
+					.setWidth(800)
+					.setHeight(400)
 					.setBackgroundColor('white');
-					
-				stockembed.setImage(chart.getUrl());
+
+				const charturl = await chart.getShortUrl();
+				stockembed.setImage(charturl);
 
 				return await interaction.reply({ embeds: [stockembed] });
 			})
--- scripts/add-stocks.js
+++ scripts/add-stocks.js
@@ -5,11 +5,11 @@ const newDoc = new schema({
   stockID: "MC",
   stockName: "Monke Coin",
   currentprice: 10,
-  priceTable: [0, 10, 11, 12, 9, 7, ],
+  priceTable: [0, 10],
   changePercent: 0,
   volume: 0,
   health: 100,
-  instability: 3,
+  volatility: 3
 });
 newDoc.save().catch(err => console.log(err)).then(() => {
   console.log("Added Monke Coin to database");
