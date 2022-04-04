56ecd001bf792b50fd6ee19d8a92aca44c37b4c3
SathvikHegdee <sathushirnika@gmail.com>
2022-04-04 21:49:43 +0530

    Make the default item potato instead of poop

--- commands/bal.js
+++ commands/bal.js
@@ -63,7 +63,7 @@ module.exports = {
             bank: 0,
             job: "Unemployed",
             workxp: 0,
-            inventory: [{ name: "Poop", count: 1 , itemType: "Consumable" }]
+            inventory: [{ name: "Golden Potato", count: 1 , itemType: "Consumable" }]
           });
           newDoc.save().catch(err => console.log(err));
 
