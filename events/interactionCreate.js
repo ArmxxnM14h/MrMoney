e813723f90dd8b9e3abba154ff2d45e52b29c659
Armxxn34 <armaanmiah2092@gmail.com>
2023-01-01 21:37:46 +0000

    not fully working interactionCreate

---
+++ events/interactionCreate.js
@@ -0,0 +1,56 @@
+module.exports = {
+    name: 'interactionCreate',
+    once: false,
+ async execute(client, interaction) {
+
+    const { Collection } = require('discord.js');
+    const fs = require('fs');
+
+
+    client.commands = new Collection();
+    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const command = require(`../commands/${file}`);
+        client.commands.set(command.data.name, command);
+    }
+            const command = client.commands.get(interaction.commandName);
+        
+        
+            if (!command) {
+                interaction.reply({ content: "Command is not found, for more info please visit => https://discord.gg/7EZV3BUDNx", ephemeral: true })
+            }
+            const timer = ms => new Promise(res => setTimeout(res, ms));
+        
+            if (command.cooldowns != undefined && command.cooldown == undefined || command.cooldowns == undefined && command.cooldown != undefined) {
+                console.error(`${command.data.name} has not got a cooldowns list but does have a cooldown. Fix this.`)
+            }
+            if (command.cooldowns != undefined) {
+                if (command.cooldowns.has(interaction.member.id)) {
+                    const cooldowntime = prettyMilliseconds(command.cooldown * 1000)
+                    const CooldownEmbed = new Discord.MessageEmbed()
+                        .setTitle('CoolDown alert!')
+                        .setDescription(`Seems your on cooldown, you only need to wait ${cooldowntime}`)
+                        .setColor('RANDOM')
+                    return await interaction.reply({ embeds: [CooldownEmbed] })
+                }
+            }
+        
+        
+            try {
+                await command.execute(interaction);
+                if (command.cooldowns != undefined && command.cooldown != undefined) {
+        
+                    command.cooldowns.add(interaction.member.id)
+        
+                    await timer(command.cooldown * 1000)
+        
+                    command.cooldowns.delete(interaction.member.id)
+                }
+            } catch (error) {
+                console.error(error)
+                return interaction.reply({ content: 'There was an error while executing this command!\n If you want, you can send the following error to our support server.\n``` js ' + error + '```', ephemeral: true, });
+        
+            }
+        
+    }
+}
--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const Discord = require('discord.js')
 const fs = require('fs');
-const { Client, Collection, Intents } = require('discord.js');
+const { Client, Intents } = require('discord.js');
 const { token, guildId } = require('./config.json');
 const prettyMilliseconds  = require("pretty-ms");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 
-client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.data.name, command);
-}
 
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
@@ -24,47 +18,7 @@ for (const file of eventFiles) {
     }
 }
 
-client.on('interactionCreate', async interaction => {
-    const command = client.commands.get(interaction.commandName);
 
-
-
-    if (!command) {
-        interaction.reply({ content: "Command is not found, for more info please visit => https://discord.gg/7EZV3BUDNx", ephemeral: true })
-    }
-    const timer = ms => new Promise(res => setTimeout(res, ms));
-
-    if (command.cooldowns != undefined && command.cooldown == undefined || command.cooldowns == undefined && command.cooldown != undefined) {
-        console.error(`${command.data.name} has not got a cooldowns list but does have a cooldown. Fix this.`)
-    }
-    if (command.cooldowns != undefined) {
-        if (command.cooldowns.has(interaction.member.id)) {
-            const cooldowntime = prettyMilliseconds(command.cooldown * 1000)
-            const CooldownEmbed = new Discord.MessageEmbed()
-                .setTitle('CoolDown alert!')
-                .setDescription(`Seems your on cooldown, you only need to wait ${cooldowntime}`)
-                .setColor('RANDOM')
-            return await interaction.reply({ embeds: [CooldownEmbed] })
-        }
-    }
-
-
-    try {
-        await command.execute(interaction);
-        if (command.cooldowns != undefined && command.cooldown != undefined) {
-
-            command.cooldowns.add(interaction.member.id)
-
-            await timer(command.cooldown * 1000)
-
-            command.cooldowns.delete(interaction.member.id)
-        }
-    } catch (error) {
-        console.error(error)
-        return interaction.reply({ content: 'There was an error while executing this command!\n If you want, you can send the following error to our support server.\n``` js ' + error + '```', ephemeral: true, });
-
-    }
-});
 
 client.login(token);
 require('./utils/updateStock.js');
