8f0c53e5cbc6c8c0b6f4fda004dce8787ed8f0f3
Armxxn34 <armaanmiah2092@gmail.com>
2023-06-24 15:13:25 +0100

    r

--- commands/give.js
+++ commands/give.js
@@ -27,81 +27,67 @@ module.exports = {
   async execute(interaction) {
     const user = interaction.options.getUser("user");
     const given = interaction.options.getInteger("amount");
-    const res = await schema.findOne({ userID: interaction.user.id })
- 
-      if (!res){
+    const res = await schema.findOne({ userID: interaction.user.id });
+  
+    if (!res) {
       const errEmbed = new EmbedBuilder()
-      .setTitle('Error')
-      .setDescription('An error has occured')
-      .setFooter('Contact Support.')
-      .setColor('Red')
-       interaction.reply({embeds: [errEmbed], ephemeral: true})
-      } 
-
-      const bal = res.coins
-
-      if (given <= 0) {
-        const AnotherOne = new EmbedBuilder()
-          .setTitle("Error")
+        .setTitle("Error")
+        .setDescription("An error has occurred")
+        .setFooter("Contact Support.")
+        .setColor("Red");
+      return interaction.reply({ embeds: [errEmbed], ephemeral: true });
+    }
+  
+    const bal = res.coins;
+  
+    if (given <= 0) {
+      const AnotherOne = new EmbedBuilder()
+        .setTitle("Error")
+        .setDescription("You cannot give anything less than one!")
+        .setColor("Red");
+      return interaction.reply({ embeds: [AnotherOne], ephemeral: true });
+    } else if (user.id === interaction.user.id) {
+      const embed = new EmbedBuilder()
+        .setTitle("Transfer Failed")
+        .setDescription("You cannot give cash to yourself")
+        .setColor("Red");
+      return interaction.reply({ embeds: [embed] });
+    } else if (bal < given) {
+      const ErrorEmbed = new EmbedBuilder()
+        .setTitle("Error In Transaction")
+        .setDescription("Your balance is below that amount")
+        .setColor("Red");
+      return interaction.reply({ embeds: [ErrorEmbed], ephemeral: true });
+    } else if (bal >= given) {
+      const res2 = await schema.findOne({ userID: user.id });
+  
+      if (!res2) {
+        const errEmbed = new EmbedBuilder()
+          .setColor("Red")
+          .setDescription(`${user.username} hasn't used the bot yet!`)
+          .setTimestamp();
+        return interaction.reply({ embeds: [errEmbed] });
+      } else {
+        res2.coins += given;
+        await res2.save();
+        res.coins -= given;
+        await res.save();
+  
+        const deposit = res.coins;
+        const balEmbed = new EmbedBuilder()
+          .setColor("Green")
+          .setTitle(`Donation Time!`)
           .setDescription(
-            `You cannot give anything less then one!`
+            `> ${user.username} has received $${given}\n\n> Given by ${interaction.user.username}`
           )
-          .setColor("Red");
-
-        await interaction.reply({ embeds: [AnotherOne], ephemeral: true });
-
-      } else if (user.id === interaction.user.id) {
-        const embed = new EmbedBuilder()
-          .setTitle('Transfer Failed')
-          .setDescription('You cannot give cash to yourself')
-          .setColor('Red')
-        await interaction.reply({ embeds: [embed] })
-
-      } else if (bal < given) {
-        const ErrorEmbed = new EmbedBuilder()
-          .setTitle("Error In Transaction")
-          .setDescription(
-            "Your balance is below that amount"
-          )
-          .setColor("Red");
-
-        await interaction.reply({ embeds: [ErrorEmbed], ephemeral: true });
-      } else if (bal >= given) {
-        // Entirely new embed
-       const res2 =  schema.findOne({ userID: user.id })
-        
-
-          if (!res2) {
-            const errEmbed = new EmbedBuilder()
-              .setColor("Red")
-              .setDescription(`${user.username} hasn't used the bot yet!`)
-              .setTimestamp();
-
-            // Reply to the entire interaction
-            return interaction.reply({ embeds: [errEmbed] });
-          } else {
-            res2.coins = res2.coins + given
-            res2.save().catch((err) => console.log(err));
-            res.coins = res.coins - given
-            res.save().catch(err => console.log(err));
-            const deposit = res.coins
-            const balEmbed = new EmbedBuilder()
-              .setColor("Green")
-              .setTitle(`Donation Time!`)
-              .setDescription(
-                `> ${user.username} has recieved $${given}
-
-                  > Given by ${interaction.user.username}
-
-                  `
-              )
-              .setFooter({
-                text: `Current balance: $${deposit}`,
-                iconURL: interaction.user.displayAvatarURL(),
-              })
-              .setTimestamp();
-            interaction.reply({ embeds: [balEmbed] });
-          }
-        };
+          .setFooter({
+            text: `Current balance: $${deposit}`,
+            iconURL: interaction.user.displayAvatarURL(),
+          })
+          .setTimestamp();
+  
+        return interaction.reply({ embeds: [balEmbed] });
       }
     }
+  }
+}  
