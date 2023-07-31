43a121c5c4f69c6c446da45c98424da6b85c6fd5
Armxxn34 <armaanmiah2092@gmail.com>
2023-07-31 20:36:55 +0100

    testing main branch deployment

--- models/userschema.js
+++ models/userschema.js
@@ -4,13 +4,14 @@ const userschema = mongoose.Schema({
   userID: String,
   userName: String,
   coins: { type: Number, default: 100 },
-  bank: { type: Number,  default: 0 },
+  bank: { type: Number, default: 0 },
   job: { type: String, default: "unemployed" },
   workxp: { type: Number, default: 0 },
   passive: { type: String, default: "disabled" },
-  inventory: [{ name: String, count: Number, itemType: String }], //its named as "itemType" instead of just "type" is because it is a reserved word in mongoose
+  inventory: [{ name: String, count: Number, itemType: String }], // It's named as "itemType" instead of just "type" because "type" is a reserved word in mongoose
   acceptedTos: { type: Boolean, default: false },
+  totalJobLevel: { type: Number, default: 0 } // Add the totalJobLevel field
 });
 
 module.exports = mongoose.model("User", userschema);
-console.log('UserSchema loaded')
+console.log('UserSchema loaded');
