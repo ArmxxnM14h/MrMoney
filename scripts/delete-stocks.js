887ae42afb5dda487e526d3b906bffee998d1b24
Armxxn34 <armaanmiah2092@gmail.com>
2023-08-13 15:13:03 +0100

    cop or drop

--- scripts/delete-stocks.js
+++ scripts/delete-stocks.js
@@ -1,12 +1,32 @@
-const Stock = require('../models/stockschema');
-async function deleteStockData() {
-    try {
-      await Stock.deleteMany({});
-      console.log('All stock data deleted successfully.');
-    } catch (error) {
-      console.error('Error deleting stock data:', error);
+const mongoose = require('mongoose');
+
+async function main() {
+  try {
+    // Connect to the MongoDB database
+    await mongoose.connect('mongodb://admin:armaanmiah2@144.172.80.146:1027/', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    // Function to delete the stock collection
+    async function deleteStockCollection() {
+      try {
+        await mongoose.connection.db.dropCollection('stocks');
+        console.log('Stock collection deleted successfully.');
+      } catch (error) {
+        console.error('Error deleting stock collection:', error);
+      }
     }
+
+    // Call the function to delete the stock collection
+    await deleteStockCollection();
+
+    // Close the Mongoose connection
+    await mongoose.connection.close();
+  } catch (error) {
+    console.error('Error:', error);
   }
-  
-  // Call the function to delete stock data
-  deleteStockData();
+}
+
+// Call the main function to initiate the process
+main();
